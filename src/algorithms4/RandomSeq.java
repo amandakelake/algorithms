ad17659bb262a9d77acbffd2f4fa581e89f81c6c
amandakelake <930007017@qq.com>
2020-04-22 13:12:42 +0800

    feat: add Scanner

--- src/algorithms4/RandomSeq.java
+++ src/algorithms4/RandomSeq.java
@@ -3,14 +3,27 @@ package algorithms4;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
+import java.util.Scanner;
+
 public class RandomSeq {
     public static void main(String[] args) {
-        int N = Integer.parseInt(args[0]);
-        double lo = Double.parseDouble(args[1]);
-        double hi = Double.parseDouble(args[2]);
-        for (int i = 0; i < N; i++) {
-            double x = StdRandom.uniform(lo, hi);
-            StdOut.printf("%.2f\n", x);
-        }
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("N:");
+            int N = scanner.nextInt();
+            System.out.println("lo:");
+            double lo = scanner.nextDouble();
+            System.out.println("hi:");
+            double hi = scanner.nextDouble();
+
+            System.out.printf("N:%d,lo:%f,hi:%f\n",N,lo,hi);
+            for (int i = 0; i < N; i++) {
+                double x = StdRandom.uniform(lo, hi);
+                StdOut.printf("%.2f\n", x);
+            }
+
+    }
+
+    private static void test(){
+        System.out.println("hello world");
     }
 }
