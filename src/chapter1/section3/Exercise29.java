4cea6d298373792e90c6ad6e64e2826c17b82514
amandakelake <930007017@qq.com>
2020-05-06 08:13:06 +0800

    feat: 1.3 round list Exercise29

---
+++ src/chapter1/section3/Exercise29.java
@@ -0,0 +1,104 @@
+package chapter1.section3;
+
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Iterator;
+import java.util.StringJoiner;
+
+public class Exercise29<Item> implements Iterable<Item> {
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    Node last;
+    int size;
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void enqueue(Item item) {
+        if (isEmpty()) {
+            last = new Node();
+            last.item = item;
+            last.next = last; // 只有一个元素，指向自己
+        } else {
+            Node node = new Node();
+            node.item = item;
+
+            if (size == 1) {
+                last.next = node;
+                node.next = last;
+            } else {
+                node.next = last.next; // 将新插入的结点指向表头
+                last.next = node; // 原先的last结点指向新的入队列的结点
+            }
+        }
+        size++;
+    }
+
+    public Item dequeue() {
+        if (isEmpty()) {
+            return null;
+        }
+        Item item;
+        if (size == 1) {
+            item = last.item;
+            last = null;
+        } else {
+            item = last.next.item; // 删除表头结点
+            last.next = last.next.next; // 将last结点指向表头第二个元素
+        }
+        size--;
+        return item;
+    }
+
+    public Iterator<Item> iterator() {
+        return new QueueIterator();
+    }
+
+    private class QueueIterator implements Iterator<Item> {
+        private Node current;
+        int count = 0;
+
+        public QueueIterator() {
+            if (last != null && size > 1) {
+                current = last.next;
+            } else {
+                current = last;
+            }
+        }
+
+        public Item next() {
+            count++;
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+
+        public boolean hasNext() {
+            return count < size;
+        }
+    }
+
+    public static void main(String[] args) {
+        Exercise29<Integer> queue = new Exercise29<>();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+
+        StringJoiner queueItems = new StringJoiner(" ");
+        // TODO 遍历环形链表的顺序 从last开始？
+        for(int item: queue) {
+            queueItems.add(String.valueOf(item));
+        }
+
+        StdOut.println("Queue items: " + queueItems.toString());
+    }
+}
