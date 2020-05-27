1f723e03848ee3721e6c1245d254353bcd0d4a8b
皛白 <xiaobai@fordeal.com>
2020-05-27 09:09:54 +0800

    feat: 2.1 Selection sort

---
+++ README.md
@@ -0,0 +1,34 @@
+# 算法
+
+线上版本 [https://algs4.cs.princeton.edu/home/](https://algs4.cs.princeton.edu/home/)
+
+## 二、排序
+[Sorting Algorithms Animations](https://www.toptal.com/developers/sorting-algorithms) 综合比较各种算法的速度
+
+
+### 1、初级排序
+* 选择排序
+* 插入排序
+### 2、归并排序
+### 3、快速排序
+### 4、优先队列
+
+## 三、查找
+
+## 四、图
+
+## 五、字符串
+
+## 六、参考资料
+
+### 6.1 可视化网站
+* [algorithm-visualizer](https://algorithm-visualizer.org/)
+该有的都有，重点是有参考代码，有C++、Java、Javascript三种版本
+
+* [visualgo](https://visualgo.net/zh)
+有复杂度备注、图形可操控调整、界面比较美观
+
+* [Data Structure Visualizations](https://www.cs.usfca.edu/~galles/visualization/Algorithms.html)
+国外某知名大学的教学工具，界面较简陋，但经典
+
+* [Sorting.at](http://sorting.at/) 小球动画
--- src/chapter2_sorting/Selection.java
+++ src/chapter2_sorting/Selection.java
@@ -3,9 +3,20 @@ package chapter2_sorting;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
-public class Example {
+public class Selection {
     public static void sort(Comparable[] a) {
-        // implement algorithms here
+        // 先按升序
+        int N = a.length;
+        // 遍历a, 将a[i]与 a[i+1...N]中最小元素交换
+        for (int i = 0; i < N; i++) {
+            int min = i; // 最小元素索引
+            for (int j = i+1; j < N; j++) {
+                if (less(a[j], a[min])) {
+                    min = j;
+                }
+            }
+            exch(a, i, min);
+        }
     }
 
     private static boolean less(Comparable v, Comparable w) {
@@ -27,7 +38,7 @@ public class Example {
 
     private static boolean isSorted(Comparable[] a) {
         for (int i = 0; i < a.length; i++) {
-            if (less(a[i], a[i-1])) return false;
+            if (less(a[i], a[i - 1])) return false;
         }
         return true;
     }
